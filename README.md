eb387e8ec7443287373cf799437ae45db0287832
Ángel Fernández Ríos <afernandezr@externos.itnow.es>
2025-03-11 12:19:45 +0100

    Adding samples for different types of tests

--- README.md
+++ README.md
@@ -7,22 +7,35 @@ The tool is based on Grafana k6
 
 ## Functionality
 
-It simulates requests to several endpoint of my application.
+It executes different load tests for a given application. Currently the code includes 3 types:
+- Ramp-Up Test --> Progressive load (ideal for testing scalability)
+- Stress Test --> Sustained load over time (ideal for testing app limits & performance degradation)
+- Spikes --> High punctual load (ideal for testing resiliency)
 
-## Repo structure
-TBD
-
-## Considerations
--
+> [!TIP]
+> Uncomment the code area of one of the test, or create the test stages from scratch (the ones are simple guidelines to start working with it). One example:
+> ```js
+> /* Option 3: Spike Test */
+> /*
+> export const options = {
+>   stages: [
+>     { duration: '1m', target: 50 },  // First 50 users for 1 minute
+>     { duration: '1m', target: 700 }, // Increase to 500 users for 1 minute
+>     { duration: '1m', target: 50 },  // Decrease to 50 users for 1 minute
+>     { duration: '1m', target: 0 },   // End test
+>   ],
+> };
+> */
+> ```
 
 ## Installation
 1. Clone the repository
 ```bash
 git clone https://github.com/afernandezrios90/load-testing-app.git
 ```
-2. Adjust the parameters of the testing if desired
-3. Run using Docker compose
+2. Adjust the test and its parameters if desired
+3. Run using Docker compose (don't forget to build the image)
 ```bash
-docker-compose up -d
+docker-compose up --build -d
 ```
 4. Review the test results
--- load-test.js
+++ load-test.js
@@ -1,11 +1,45 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-export let options = {
-  vus: 10, // 1 virtual user
-  duration: '1m', // Execute for 1 minute
-  iterations: 30, // Repeat 10 times
+// Section to define the test configuration in term of stages, duration, virtual users and iterations
+
+// SELECT THE TEST CONFIGURATION
+// Uncomment one of the following options to select the desired test configuration
+
+/* Option 1: Ramp-Up Test */
+export const options = {
+  stages: [
+    { duration: '30s', target: 10 }, // 10 users for 30 seconds
+    { duration: '30s', target: 50 }, // 50 users for 30 seconds
+    { duration: '30s', target: 100 }, // 50 users for 30 seconds
+    { duration: '30s', target: 0 }, // no users for 30 seconds
+  ],
 };
+
+/* Option 2: Stress Test */
+/*
+export const options = {
+  stages: [
+    { duration: '1m', target: 50 },  // First 50 users for 1 minute
+    { duration: '2m', target: 200 }, // Increase to 200 users for 2 minutes
+    { duration: '2m', target: 500 }, // Increase to 500 users for 2 minutes
+    { duration: '2m', target: 50 },  // Decrease to 50 users for 2 minutes to see recovery
+    { duration: '1m', target: 0 },   // End test
+  ],
+};
+*/
+
+/* Option 3: Spike Test */
+/*
+export const options = {
+  stages: [
+    { duration: '1m', target: 50 },  // First 50 users for 1 minute
+    { duration: '1m', target: 700 }, // Increase to 500 users for 1 minute
+    { duration: '1m', target: 50 },  // Decrease to 50 users for 1 minute
+    { duration: '1m', target: 0 },   // End test
+  ],
+};
+*/
 
 export default function () {
   const endpoints = [
@@ -21,5 +55,4 @@ export default function () {
     'is status 200': (r) => r.status === 200 || r.status === 404, // Check if the status code is 200 or 404
   });
 
-  sleep(2);  // Wait for 6 seconds before making the next request
 }
